c3703fe20bebf61a87f0288b6ddb55791d17972c
KhanhNamYeh <523H0148@student.tdtu.edu.vn>
2025-05-08 17:59:16 +0700

    easy to run

--- server.js
+++ server.js
@@ -27,4 +27,5 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {console.log(`Server running at http://localhost:${PORT}/`);});
+
+app.listen(PORT, () => {console.log(`Server running at http://localhost:${PORT}/ Ctrl + click that link`);});
